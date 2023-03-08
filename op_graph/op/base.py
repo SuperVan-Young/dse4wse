72e66d833b43fb8d34c3c2261baed6d1d4959383
Chenhao Xue <xch927027@pku.edu.cn>
2023-03-08 10:45:35 +0800

    reorganize base op

--- op_graph/op/base.py
+++ op_graph/op/base.py
@@ -6,71 +6,77 @@ from itertools import chain
 from op_graph import SbpSignature
 from utils import ArchConfig
 
-# Estimation level of operator's latency
-ESTIMATE_COMM = 1
-ESTIMATE_COMP_COMM = 2
-
 class Operator(ABC):
 
     def __init__(self, 
                 name: str, 
                 op_type: str, 
-                input_tensors: Dict[str, str],
-                output_tensors: Dict[str, str],
-                shapes: Dict[str, Tuple[int]],
+                input_tensors: Dict[str, str],  # param -> full name
+                output_tensors: Dict[str, str], # param -> full name
                 *args, **kwargs) -> None:
         self.name = name
         self.op_type = op_type
-        self.input_tensors = input_tensors
+        self.input_tensors = input_tensors 
         self.output_tensors = output_tensors
-        self.shapes = shapes
 
-        self.sbp_signatures : Dict[str, SbpSignature]
-        self.sbp_signatures = {}
+        self.num_core_lower_bound = 0  # utilized_core in [lower_bound, upper_bound]
+        self.num_core_upper_bound = 0
+        self.final_sbp_signatures : Dict[str, SbpSignature]  # full name -> sbp_signature
+        self.final_sbp_signatures = {}
+        self._candidate_sbp_signatures: List[Dict[str, SbpSignature]]
+        self._candidate_sbp_signatures = []
 
-    def estimate_cost(self, level, arch_config: ArchConfig, inter_layer_sbp: Dict[str, SbpSignature]) -> float:
-        cost = 0
-        if level == ESTIMATE_COMM:
-            cost += self._estimate_communication_cost(arch_config, inter_layer_sbp)
-        elif level == ESTIMATE_COMP_COMM:
-            cost += self._estimate_communication_cost(arch_config, inter_layer_sbp)
-            cost += self._estimate_computation_cost(arch_config)
-        else:
-            raise NotImplementedError(f"Estimation level {level} not implemented")
-        return cost
+    def estimate_communication_cost(self, arch_config: ArchConfig, inter_layer_sbp: Dict[str, SbpSignature]) -> float:
+        return self._estimate_communication_cost(arch_config, inter_layer_sbp)
     
-    @abstractmethod
-    def _estimate_communication_cost(self, arch_config: ArchConfig, inter_layer_sbp: Dict[str, SbpSignature]) -> float:
-        raise NotImplementedError
+    def estimate_computation_cost(self, arch_config: ArchConfig) -> float:
+        return self._estimate_computation_cost(arch_config)
     
-    @abstractmethod
-    def _estimate_computation_cost(self, arch_config: ArchConfig, ) -> float:
-        raise NotImplementedError
+    def estimate_memory_cost(self) -> float:
+        return self._estimate_memory_cost()
     
-    def derive_output_shapes(self) -> None:
+    def derive_output_shapes(self, input_shapes: Dict[str, Tuple[int]]) -> Dict[str, Tuple[int]]:
         for inp in self.input_tensors.values():
-            assert inp in self.shapes, f"Missing shape of input tensor {inp}"
-        self._derive_output_shapes()
-        for oup in self.output_tensors.values():
-            assert inp in self.shapes, f"Missing shape of output tensor {oup}"
-
-    @abstractmethod
-    def _derive_output_shapes(self) -> None:
-        raise NotImplementedError
+            assert inp in input_shapes, f"Missing shape of input tensor {inp}"
+        return self._derive_output_shapes(input_shapes)
     
-    def find_best_sbp_signature(self, 
-                                max_devices: int, 
-                                inter_layer_sbp: Dict[str, SbpSignature] = {}
+    def derive_candidate_sbp_signatures(self):
+        assert self.num_core_lower_bound != 0
+        assert self.num_core_upper_bound != 0
+        self._candidate_sbp_signatures = []
+        self._derive_candidate_sbp_signatures()
+        assert len(self._candidate_sbp_signatures) > 0
+    
+    def find_best_sbp_signature(self, inter_layer_sbp: Dict[str, SbpSignature] = {}
                                 ) -> Dict[str, SbpSignature]:
-        sbp = self._find_best_sbp_signature(max_devices, inter_layer_sbp)
+        assert self._candidate_sbp_signatures, "Derive candidate sbp signatures first!"
+        sbp = self._find_best_sbp_signature(inter_layer_sbp)
         for t in chain(self.input_tensors.values(), self.output_tensors.values()):
             if t not in sbp:
                 raise ValueError(f"{t} not in SBP signatures")
         return sbp
     
     @abstractmethod
-    def _find_best_sbp_signature(self,
-                                max_devices: int, 
-                                inter_layer_sbp: Dict[str, SbpSignature] = {}
+    def _estimate_communication_cost(self, arch_config: ArchConfig, inter_layer_sbp: Dict[str, SbpSignature]) -> float:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _estimate_computation_cost(self, arch_config: ArchConfig) -> float:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _estimate_memory_cost(self) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _derive_output_shapes(self) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _derive_candidate_sbp_signature():
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _find_best_sbp_signature(self, inter_layer_sbp: Dict[str, SbpSignature] = {}
                                 ) -> Dict[str, SbpSignature]:
         raise NotImplementedError
