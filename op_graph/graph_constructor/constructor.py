27469172b98e52bc60c35ea82bd55e9561e6922a
Chenhao Xue <xch927027@pku.edu.cn>
2023-03-10 15:25:11 +0800

    bert graph constructor

--- op_graph/graph_constructor/constructor.py
+++ op_graph/graph_constructor/constructor.py
@@ -1,32 +1,65 @@
+import torch
+import onnx
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from utils import logger
 from op_graph import OpGraph
 from op_graph.op import build_operator
-from oneflow.core.job.job_pb2 import Job
+
+from onnx.shape_inference import infer_shapes
+from onnxsim import simplify
+from transformers import BertTokenizer, BertModel
+
+MODEL_CACHE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
+if not os.path.exists(MODEL_CACHE_DIR):
+    os.mkdir(MODEL_CACHE_DIR)
 
 class OpGraphConstructor():
+    """Build OpGraph from ONNX models
+    """
 
     def __init__(self) -> None:
         pass    
         
-    def build_op_graph(self, graph_name: str) -> OpGraph:
-        oneflow_graph_proto = self._get_graph_proto(graph_name)
-        op_graph = self._build_from_graph_proto(oneflow_graph_proto)
+    def build_op_graph(self, model_name: str) -> OpGraph:
+        onnx_model = self._get_onnx_model(model_name)
+        op_graph = self._build_from_onnx_model(onnx_model)
         return op_graph
     
-    def _get_graph_proto(self, graph_name: str) -> Job:
-        # TODO: add graph proto
-        raise NotImplementedError
+    def _get_onnx_model(self, model_name: str):
+        model_path = os.path.join(MODEL_CACHE_DIR, f"{model_name}.onnx")
+        model_2_build_func = {
+            'bert_model': build_bert_model,
+        }
 
-    def _build_from_graph_proto(self, graph_proto: Job) -> OpGraph:
-        assert isinstance(graph_proto, Job), f"Ivalid type {type(graph_proto)} for graph_proto"
+        if not os.path.exists(model_path):
+            try:
+                build_func = model_2_build_func[model_name]
+                build_func()
+            except KeyError:
+                logger.critical("We don't have onnx model %s" % (model_name, ))
+                exit(1)
+
+        with open(model_path, 'rb') as f:
+            model = onnx.load(f)
+        model, check = simplify(model)
+        assert check, "Failed to simplify model %s" % (model_name,)
+        model = infer_shapes(model)
+        return model
+
+
+    def _build_from_onnx_model(self, onnx_model) -> OpGraph:
         op_graph = OpGraph()
 
         # add operators from graph proto
-        for op_conf in graph_proto.net.op:
-            name = op_conf.name
+        for op_proto in onnx_model.graph.node:
+            name = op_proto.name
             try:
-                op_node = build_operator(op_conf)
+                op_node = build_operator(op_proto)
             except NotImplementedError:
                 logger.debug(f"Ignoring operator {name} due to missing implementation.")
                 continue
@@ -42,7 +75,24 @@ class OpGraphConstructor():
                 if used_tensors:
                     op_graph.add_edge(u, v, used_tensors=used_tensors)
         
-        # derive tensor shapes
-        #TODO: finish this part
+        op_graph._onnx_model = onnx_model
 
         return op_graph
+    
+
+def build_bert_model():
+    """Bert model from huggingface.
+    Preliminaries: `pip install transformers`
+    """
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained("bert-base-uncased")
+    text = "Replace me by any text you'd like."
+    encoded_input = tokenizer(text, return_tensors='pt')
+    encoded_input_ = {k: v for k, v in encoded_input.items() if isinstance(v, (torch.Tensor))}
+
+    with open(os.path.join(MODEL_CACHE_DIR, "bert_model.onnx"), 'wb') as f:
+        torch.onnx.export(model, args=(encoded_input_,), f=f)
+
+if __name__ == "__main__":
+    constructor = OpGraphConstructor()
+    constructor.build_op_graph("bert_model")
