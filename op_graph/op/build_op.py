c90030e95977828fbdad9b03ef488963d7c9908b
Chenhao Xue <xch927027@pku.edu.cn>
2023-03-17 17:18:49 +0800

    register binary op

--- op_graph/op/build_op.py
+++ op_graph/op/build_op.py
@@ -6,16 +6,17 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from op_graph.op import (
-    Operator,
-    UnaryElementwiseOperator
-)
 from utils import logger, TensorInfo
+
+from base import Operator
+from unary_elementwise import UnaryElementwiseOperator
+from binary_elementwise import BinaryElementwiseOperator
 
 def build_operator(name: str, op_type: str, 
                    input_tensors: List[TensorInfo], 
                    output_tensors: List[TensorInfo]) -> Operator:
     
+    # Unary Elementwise Operators
     if op_type == 'Log':
         input_tensors = {'in': input_tensors[0]}
         output_tensors = {'out': output_tensors[0]}
@@ -24,6 +25,16 @@ def build_operator(name: str, op_type: str,
         input_tensors = {'in': input_tensors[0]}
         output_tensors = {'out': output_tensors[0]}
         op = UnaryElementwiseOperator(name, op_type, input_tensors, output_tensors, operation_intensity=10)
+    
+    # Binary Elementwise Operators
+    elif op_type in ['Add', 'Sub', 'Mul', 'Div', 'Pow']:
+        input_tensors = {
+            'A': input_tensors[0],
+            'B': input_tensors[1],
+        }
+        output_tensors = {'out': output_tensors[0]}
+        #TODO: measure realy operation intensity
+        op = BinaryElementwiseOperator(name, op_type, input_tensors, output_tensors, operation_intensity=1)
     else:
         raise NotImplementedError
     return op
