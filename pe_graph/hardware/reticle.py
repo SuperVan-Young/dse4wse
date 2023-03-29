1c55c7c86b3fc4c01c1ce032b95e82e572d7d39b
Chenhao Xue <xch927027@pku.edu.cn>
2023-03-29 19:50:49 +0800

    rebuild wafer

--- pe_graph/hardware/reticle.py
+++ pe_graph/hardware/reticle.py
@@ -6,5 +6,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
 class ReticleArray():
-    def __init__(self) -> None:
+    def __init__(self, **kwargs) -> None:
+        self.coordinate = kwargs.get('coordinate', None)
         pass
--- pe_graph/hardware/wafer.py
+++ pe_graph/hardware/wafer.py
@@ -32,73 +32,61 @@ class WaferScaleEngine():
         self._reticle_graph = self.__build_reticle_graph(reticle_config)
     
     def __build_reticle_graph(self, reticle_config: Dict):
-        G = nx.DiGraph()
+        # build graph skeleton
+        H, W = self.reticle_array_height, self.reticle_array_width
+        G = nx.grid_2d_graph(m=range(-1, H+1), n=range(-1, W+1), create_using=nx.DiGraph)
+        G : nx.DiGraph
+        for node in [(-1, -1), (H, -1), (-1, W), (H, W)]:
+            G.remove_node(node)
+        for node, ndata in G.nodes(data=True):
+            ndata['reticle'] = None
+            ndata['dram_port'] = None
 
         def add_reticle(x, y, reticle: ReticleArray):
-            G.add_node(self.__reticle_naming(x, y), hw_type='reticle', coordinate=(x,y), reticle=reticle)
+            G.nodes[(x, y)]['reticle'] = reticle
 
         def add_dram_port(x, y, dram_port: DramPort):
-            G.add_node(self.__dram_port_naming(x, y), hw_type='dram_port', coordinate=(x,y), dram_port=dram_port)
-
-        def add_reticle_2_reticle_bidirectional_link(x1, y1, x2, y2):
-            G.add_edge(self.__reticle_naming(x1, y1), self.__reticle_naming(x2, y2))
-            G.add_edge(self.__reticle_naming(x2, y2), self.__reticle_naming(x1, y1))
-        
-        def add_port_2_reticle_bidirectional_link(xp, yp, xr, yr):
-            G.add_edge(self.__dram_port_naming(xp, yp), self.__reticle_naming(xr, yr))
-            G.add_edge(self.__reticle_naming(xr, yr), self.__dram_port_naming(xp, yp))
+            G.nodes[(x, y)]['dram_port'] = dram_port
 
         # reticle arrays
-        for x, y in product(range(self.reticle_array_height), range(self.reticle_array_width)):
+        for x, y in product(range(H), range(W)):
             reticle = ReticleArray(coordinate=(x, y), **reticle_config)
             add_reticle(x, y, reticle)
-        for x, y in product(range(self.reticle_array_height - 1), range(self.reticle_array_width)):
-            add_reticle_2_reticle_bidirectional_link(x-1, y, x, y)
-        for x, y in product(range(self.reticle_array_height), range(self.reticle_array_width - 1)):
-            add_reticle_2_reticle_bidirectional_link(x, y-1, x, y)
 
         # dram ports
         if self.dram_stacking_type == '2d':
             for x in range(0, self.reticle_array_height):
                 add_dram_port(x, -1, DramPort())
                 add_dram_port(x, self.reticle_array_width, DramPort())
-                add_port_2_reticle_bidirectional_link(x, -1, x, 0)
-                add_port_2_reticle_bidirectional_link(x, self.reticle_array_width, x, self.reticle_array_width-1)
             for y in range(0, self.reticle_array_width):
                 add_dram_port(-1, y, DramPort())
                 add_dram_port(self.reticle_array_height, y, DramPort())
-                add_port_2_reticle_bidirectional_link(-1, y, 0, y)
-                add_port_2_reticle_bidirectional_link(self.reticle_array_height, y, self.reticle_array_height-1, 0)
         elif self.dram_stacking_type == '3d':
             for x, y in product(range(self.reticle_array_height), range(self.reticle_array_width)):
                 add_dram_port(x, y, DramPort())
-                add_port_2_reticle_bidirectional_link(x, y, x, y)
         else:
             raise NotImplementedError
 
         return G
-
+    
+    def get_node_from_coordinate(self, x: int, y: int):
+        return self.nodes([x, y])  # for direct graph operations
             
     def get_reticle_from_coordinate(self, x: int, y: int) -> ReticleArray:
         assert x >= 0 and x < self.reticle_array_height
         assert y >= 0 and y < self.reticle_array_width
-        return self._reticle_graph.nodes[self.__reticle_naming(x, y)]['reticle']
+        ret = self._reticle_graph.nodes[(x, y)]['reticle']
+        assert ret
+        return ret
     
     def get_dram_port_from_coordinate(self, x: int, y: int) -> DramPort:
         if self.dram_stacking_type == '2d':
             assert x in [-1, self.reticle_array_height] or y in [-1, self.reticle_array_width]
-            return self._reticle_graph.nodes[self.__dram_port_naming(x, y)]['dram_port']
+            ret = self._reticle_graph.nodes[(x, y)]['dram_port']
         elif self.dram_stacking_type == '3d':
             assert x in range(self.reticle_array_height) and y in range(self.reticle_array_width)
-            return self._reticle_graph.nodes[self.__dram_port_naming(x, y)]['dram_port']
+            ret = self._reticle_graph.nodes[(x, y)]['dram_port']
         else:
             raise RuntimeError
-
-    def __reticle_naming(self, x: int, y: int) -> str:
-        return f"r_{x}_{y}"
-    
-    def __dram_port_naming(self, x: int, y: int) -> str:
-        return f"d_{x}_{y}"
-    
-    def get_dram_port_coordinate_list(self) -> List[DramPort]:
-        return [ndata['coordinate'] for name, ndata in self._reticle_graph.nodes(data=True) if ndata['hm_type'] == 'dram_port']
+        assert ret
+        return ret
